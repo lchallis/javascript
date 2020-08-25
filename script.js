f499d66a87cd6c7ad576bc2ff1685627082cbd48
Lachie <you@example.com>
2020-08-25 16:44:14 +1000

    done

---
+++ script.js
@@ -0,0 +1,70 @@
+var students = [
+    {
+        name: 'John',
+        score1: 47,
+        score2: 46
+    },
+    {
+        name: 'Bob',
+        score1: 23,
+        score2: 24
+    },
+    {
+        name: 'Nick',
+        score1: 40,
+        score2: 35
+    },
+    {
+        name: 'Alex',
+        score1: 44,
+        score2: 45
+    }
+];
+
+var overallScores = [
+    {
+        score: 91,
+        grade: 'A'
+    },
+    {
+        score: 81,
+        grade: 'B'
+    },
+    {
+        score: 71,
+        grade: 'C'
+    },
+    {
+        score: 61,
+        grade: 'D'
+    },
+    {
+        score: 51,
+        grade: 'E'
+    }
+];
+
+function calcSum(score1, score2) {
+    var sum = score1 + score2;
+    return sum;
+}
+
+function judge(student) {
+    var totalScore = calcSum(student.score1, student.score2);
+    if (totalScore >= 51) {
+        for (var x = 0; x < overallScores.length; x++) {
+            if (totalScore >= overallScores[x].score) {
+                console.log(student.name + " has a score of " + totalScore + " they get a " + overallScores[x].grade + " Grade")
+                break;
+            }
+        }
+    }
+    else {
+        console.log(student.name + " is a gronk and failed with a score of " + totalScore);
+    }
+}
+
+for (var i = 0; i < students.length; i++) {
+    var student = students[i];
+    judge(student)
+}
